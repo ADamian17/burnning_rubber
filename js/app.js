095f06236eb410e9ce1b784a0b6c9eb9aaa9d6f1
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-02 18:20:23 -0700

    add some animation

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ console.log('Hello Adonis!')
 
 //--- When the user click START, the user should see their car in the game window and the timer should start running.
 //--- The user will have to avoid the cars coming in the opposite directtion.
-//--- the user will used the used the arrows to control the car 
+//--- the user will control the car by using the A and D. 
 //--- the User will get 5 points, everytime  he or she avoids an obstacle. 
 //--- Each level is going last 40 sec. 
 //--- the obstacle and the speed will increase as the levels gets higher. 
@@ -32,7 +32,7 @@ $(document).ready(function () {
      
     let start = $('<button class="start">Start</button>');
     $("#gameArea").append(start);
-    $(start).click(function (a) {
+    $(start).on('click', function (a) {
         createAuto();
         $(start).hide();
     });
@@ -44,20 +44,16 @@ $(document).ready(function () {
 
 //-------- main car ------------------///
 
-createAuto = () => {
+const createAuto = () => {
     let $player1 = $('<div class="player"></div>'); /* Utilices un objeto */
-    // $(player1).css('background', 'red');
      $($player1).css('bottom', '3%');
-    $($player1).css('left', '45%');
+     $($player1).css('left', '45%');
 
 
 
     $("#gameArea").append($player1);
 
     $( "body" ).keypress(function( event ) {
-        let $anchoRoad = $("#gameArea").width();
-        let $anchoPlayer = $($player1).width();
-        
 
         if ( event.which == 97 ) {
             let left = $($player1).position().left;
@@ -74,7 +70,31 @@ createAuto = () => {
                 $($player1).css('left', right + "px");
             }
         }
+        
     });
+
+    obstacles();
 }
 
 //--------------------------------------------------------//
+
+let truck1 = $('<div class="cars yellow"></div>'); 
+let camaro = $('<div class="cars camaro"></div>'); 
+let police = $('<div class="cars police"></div>'); 
+let fireTruck = $('<div class="cars fire"></div>'); 
+let greenTruck = $('<div class="cars green"></div>'); 
+
+
+
+const obstacles = () => {
+
+    const $gameCars = [truck1, police, camaro, fireTruck, greenTruck];
+       for (let i = 0; i < $gameCars.length; i++) {
+         $($gameCars).css('top', '10%');
+         $('#gameArea').prepend($gameCars);
+       }
+  
+}
+
+
+
